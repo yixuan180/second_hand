7b14b474761fed2c354eda71e112c9396e96feda
CCin188 <chuxin918930@gmail.com>
2025-12-14 18:41:59 +0800

    Add files via upload

---
+++ secondhand_sa/secondhand_final/src/main/java/com/example/demo/Main.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Spring Boot 入口
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ secondhand_sa/secondhand_final/src/main/java/com/example/demo/model/Member.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+public class Member extends User {
+    // 擴充會員專屬欄位
+    public Member(String username, String password) {
+        super(username, password);
+    }
+   }
