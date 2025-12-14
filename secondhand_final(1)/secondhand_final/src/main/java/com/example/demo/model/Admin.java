4adde52a41b2b44bd27b1d6be061e144f286621b
CCin188 <chuxin918930@gmail.com>
2025-12-14 18:39:41 +0800

    Delete secondhand_final(1) directory

--- secondhand_final(1)/secondhand_final/src/main/java/com/example/demo/model/Admin.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public class Admin extends User {
-    // 擴充管理員專屬欄位
-    public Admin(String username, String password) {
-        super(username, password);
-    }
-  
-}
