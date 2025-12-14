a1b808880ce507e4b117e4fd0eb28e515052c426
CCin188 <chuxin918930@gmail.com>
2025-12-14 18:37:23 +0800

    Delete secondhand_final/secondhand_final directory

--- secondhand_final/secondhand_final/src/main/java/com/example/demo/model/Admin.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public class Admin extends User {
-    // 這裡可以擴充管理員專屬欄位
-    public Admin(String username, String password) {
-        super(username, password);
-    }
-  
-}
--- secondhand_final/secondhand_final/src/main/java/com/example/demo/model/Member.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public class Member extends User {
-    // 這裡可以擴充會員專屬欄位
-    public Member(String username, String password) {
-        super(username, password);
-    }
-   }
